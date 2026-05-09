3f52b30e9ac3dbadd671e39593cc4c345ee5d989
WILLzzz-Termux <wildangz3001@gmail.com>
2026-05-10 01:19:55 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+export default {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#030014",
+        neon: "#7f5af0"
+      }
+    }
+  },
+  plugins: []
+}
